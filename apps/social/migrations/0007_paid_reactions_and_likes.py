7662ea25fe123052159e09a5cdf71c66c7bb4018
georgetoloraia <gtoloraia19@gmail.com>
2026-01-23 21:17:32 +0400

    feat(social): add likes and paid reactions

---
+++ apps/social/migrations/0007_paid_reactions_and_likes.py
@@ -0,0 +1,169 @@
+from __future__ import annotations
+
+import django.db.models.deletion
+from django.conf import settings
+from django.db import migrations, models
+
+import libs.idgen
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("coin", "0001_initial"),
+        ("payments", "0010_gifttype_fields"),
+        ("social", "0006_postvideo"),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name="comment",
+            name="like_count",
+            field=models.PositiveIntegerField(default=0),
+        ),
+        migrations.CreateModel(
+            name="PostLike",
+            fields=[
+                (
+                    "id",
+                    models.BigIntegerField(
+                        default=libs.idgen.generate_id,
+                        editable=False,
+                        primary_key=True,
+                        serialize=False,
+                    ),
+                ),
+                ("created_at", models.DateTimeField(auto_now_add=True)),
+                ("updated_at", models.DateTimeField(auto_now=True)),
+                (
+                    "post",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        related_name="likes",
+                        to="social.post",
+                    ),
+                ),
+                (
+                    "user",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        related_name="post_likes",
+                        to=settings.AUTH_USER_MODEL,
+                    ),
+                ),
+            ],
+            options={
+                "abstract": False,
+                "unique_together": {("user", "post")},
+            },
+        ),
+        migrations.CreateModel(
+            name="CommentLike",
+            fields=[
+                (
+                    "id",
+                    models.BigIntegerField(
+                        default=libs.idgen.generate_id,
+                        editable=False,
+                        primary_key=True,
+                        serialize=False,
+                    ),
+                ),
+                ("created_at", models.DateTimeField(auto_now_add=True)),
+                ("updated_at", models.DateTimeField(auto_now=True)),
+                (
+                    "comment",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        related_name="likes",
+                        to="social.comment",
+                    ),
+                ),
+                (
+                    "user",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        related_name="comment_likes",
+                        to=settings.AUTH_USER_MODEL,
+                    ),
+                ),
+            ],
+            options={
+                "abstract": False,
+                "unique_together": {("user", "comment")},
+            },
+        ),
+        migrations.CreateModel(
+            name="PaidReaction",
+            fields=[
+                (
+                    "id",
+                    models.BigIntegerField(
+                        default=libs.idgen.generate_id,
+                        editable=False,
+                        primary_key=True,
+                        serialize=False,
+                    ),
+                ),
+                ("created_at", models.DateTimeField(auto_now_add=True)),
+                ("updated_at", models.DateTimeField(auto_now=True)),
+                (
+                    "target_type",
+                    models.CharField(
+                        choices=[("post", "Post"), ("comment", "Comment")],
+                        max_length=16,
+                    ),
+                ),
+                ("quantity", models.PositiveIntegerField(default=1)),
+                ("total_amount_cents", models.PositiveIntegerField()),
+                ("idempotency_key", models.UUIDField(blank=True, null=True, unique=True)),
+                (
+                    "coin_event",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.PROTECT,
+                        related_name="paid_reactions",
+                        to="coin.coinevent",
+                    ),
+                ),
+                (
+                    "comment",
+                    models.ForeignKey(
+                        blank=True,
+                        null=True,
+                        on_delete=django.db.models.deletion.CASCADE,
+                        related_name="paid_reactions",
+                        to="social.comment",
+                    ),
+                ),
+                (
+                    "gift_type",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.PROTECT,
+                        related_name="paid_reactions",
+                        to="payments.gifttype",
+                    ),
+                ),
+                (
+                    "post",
+                    models.ForeignKey(
+                        blank=True,
+                        null=True,
+                        on_delete=django.db.models.deletion.CASCADE,
+                        related_name="paid_reactions",
+                        to="social.post",
+                    ),
+                ),
+                (
+                    "sender",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        related_name="paid_reactions_sent",
+                        to=settings.AUTH_USER_MODEL,
+                    ),
+                ),
+            ],
+            options={
+                "abstract": False,
+                "indexes": [models.Index(fields=["target_type", "created_at"])],
+            },
+        ),
+    ]
